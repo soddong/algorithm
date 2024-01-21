166f1418a3ded45ed898b50c68959597d7b8cb8d
soddong <aledjdygus9300@gmail.com>
2024-01-21 22:39:09 +0900

    dfs

---
+++ baekjoon/15723_n단논법.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+answer = ''
+n = int(input())
+
+relation = defaultdict(list)
+checked = defaultdict(int)
+
+for _ in range(n):
+    sentence = input()
+    src, dst = sentence[0], sentence[-1]
+    relation[src].append(dst)
+    checked[dst] += 1
+
+def dfs(src, dst):
+    global answer
+    if src == dst:
+        answer = 'T'
+    for x in relation[src]:
+        if checked[x] > 0:
+            checked[x] -= 1
+            dfs(x, dst)
+            checked[x] += 1
+
+
+m = int(input())
+
+for _ in range(m):
+    sentence = input()
+    answer = 'F'
+    dfs(sentence[0], sentence[-1])
+    print(answer)
